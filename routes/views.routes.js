6eecf84a2d962ddd69ba852aad7ac2eac3ec0b93
Blake Mason <blake@Blakes-MacBook-Pro.local>
2018-08-10 15:03:48 -0700

    cleanup

--- routes/views.routes.js
+++ routes/views.routes.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 130d6dc771bb1161c7c8ffb9c316e244b38de0b9
 //requires
 var express = require('express');
 
@@ -13,8 +9,4 @@ router.get("/profile", function (req, res) {
   res.render('profile');
 })
 
-<<<<<<< HEAD
-=======
-//export
->>>>>>> 130d6dc771bb1161c7c8ffb9c316e244b38de0b9
 module.exports = router;
